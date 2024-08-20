c6dcf7f574fd703033e70b7a548fd7871ff6de3e
zeyad amer <zeyadamer285@gmail.com>
2024-08-20 17:21:54 +0300

    removed unused classes

---
+++ src/main/java/Controller/Course.java
@@ -0,0 +1,83 @@
+package Controller;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private int credit;
+
+    @ManyToMany(mappedBy = "courses")
+    private List<Author> authors;
+
+    @OneToMany(mappedBy = "course")
+    private List<Rating> ratings;
+
+    @OneToOne(mappedBy = "course")
+    private Assessment assessment;
+
+    public Course() {
+    }
+
+
+    public Course(int id, String name, String description, int credit) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.credit = credit;
+    }
+
+    public Course(String name) {
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getCredit() {
+        return credit;
+    }
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+    public List<Author> getAuthors() {
+        return authors;
+    }
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
+    public Assessment getAssessment() {
+        return assessment;
+    }
+    public void setAssessment(Assessment assessment) {
+        this.assessment = assessment;
+    }
+
+}
--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -5,40 +5,5 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 
 public class Main {
-    public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        CourseService courseService = context.getBean(CourseService.class);
-        courseService.getRecommendedCourses().forEach(course -> System.out.println(course.getName()));
-
-        courseService.addCourse("Mathematics", "Basic Math Course", 3);
-        System.out.println("Course added!");
-
-
-
-        courseService.viewAllCourses().forEach(course ->
-                System.out.println(course.getId() + ": " + course.getName() + " - " + course.getDescription() + " (" + course.getCredit() + " credits)")
-        );
-
-
-
-        courseService.updateCourse(2, "Mathematics", "Updated Math Course", 4);
-
-
-
-        Course updatedCourse = courseService.viewCourse(2);
-        System.out.println(updatedCourse.getId() + ": " + updatedCourse.getName() + " - " + updatedCourse.getDescription() + " (" + updatedCourse.getCredit() + " credits)");
-
-        // Test deleteCourse
-
-        courseService.deleteCourse(1);
-        System.out.println("Course deleted!");
-
-
-
-        courseService.viewAllCourses().forEach(course ->
-                System.out.println(course.getId() + ": " + course.getName() + " - " + course.getDescription() + " (" + course.getCredit() + " credits)")
-        );
-    }
 }
