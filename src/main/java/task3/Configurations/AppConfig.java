8ee133913a28dd702b0434d3b57a6714a8c5d720
zeyad amer <zeyadamer285@gmail.com>
2024-08-27 17:49:22 +0300

    removed database connection file that was pushed by mistake

--- src/main/java/task3/Configurations/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package task3.Configurations;
-
-import org.springframework.context.annotation.*;
-
-
-@Configuration
-@ComponentScan(basePackages = {"task3","Controller","Repositories","Services","Mappers"})
-public class AppConfig {
-}
