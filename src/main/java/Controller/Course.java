a49157d7e1db80f7e2488cb9cf3f6ca69b09d3fa
zeyad amer <zeyadamer285@gmail.com>
2024-09-09 17:59:19 +0300

    fix COurse constructor

--- src/main/java/Controller/Course.java
+++ src/main/java/Controller/Course.java
@@ -27,7 +27,7 @@ public class Course {
     }
 
 
-    public Course(int id, String name, String description, int credit) {
+    public Course(String name, String description, int credit) {
         this.id = id;
         this.name = name;
         this.description = description;
