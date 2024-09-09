7fd99c81636bebe7be9485be9ca0c1febdc18b2b
zeyad amer <zeyadamer285@gmail.com>
2024-09-09 16:47:28 +0300

    fixing code structure

--- src/main/java/task3/AppConfig.java
+++ src/main/java/task3/AppConfig.java
@@ -1,9 +1,9 @@
 package task3;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import task2.CourseRecommender;
+
 
 @Configuration
 @ComponentScan(basePackages = {"task3","task2"})
